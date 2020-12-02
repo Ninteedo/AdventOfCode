d7c645c11922231a3269df481e299a59e9f4d4f5
Richard Holmes <richard.gordon.holmes@gmail.com>
2020-12-02 11:47:30 +0000

    Small cleanup of day 1 and 2 challenges

--- Day1/ReportRepair.hs
+++ Day1/ReportRepair.hs
@@ -1,15 +1,19 @@
 f :: [Int] -> Int
-f xs = head [ xs!!i * xs!!j | i <- [0..length xs - 1], j <- [0..length xs - 1], i /= j, xs!!i + xs!!j == 2020 ]
+f xs = head [ xs!!i * xs!!j |
+              i <- [0..length xs - 1], j <- [0..length xs - 1],
+              i /= j,
+              xs!!i + xs!!j == 2020 ]
 
 g :: [Int] -> Int
-g xs = head [ xs!!i * xs!!j * xs!!k | i <- [0..length xs - 1], j <- [0..length xs - 1], k <- [0..length xs - 1], i /= j && j /= k, xs!!i + xs!!j + xs!!k == 2020 ]
-
-toInt :: String -> Int
-toInt x = read x :: Int
+g xs = head [ xs!!i * xs!!j * xs!!k |
+              i <- [0..length xs - 1], j <- [0..length xs - 1], k <- [0..length xs - 1],
+              i /= j && j /= k,
+              xs!!i + xs!!j + xs!!k == 2020 ]
 
 main = do
     contents <- readFile "Day1/input.txt"
     let ss = lines contents
-        xs = map toInt ss
-    print (f xs)
-    print (g xs)
+        xs :: [Int]
+        xs = map read ss
+    print ("#1: " ++ show (f xs))
+    print ("#2: " ++ show (g xs))
--- Day2/PasswordPhilosophy.hs
+++ Day2/PasswordPhilosophy.hs
@@ -1,21 +1,21 @@
 f :: [String] -> Int
-f ss = length $ filter checkValid ss
+f = length . filter checkValid
     where
         checkValid :: String -> Bool
         checkValid line = watchCharCount >= read lowLimit && watchCharCount <= read highLimit
             where
-                (lowLimit,_:rest) = break (== '-') line
-                (highLimit,_:watchChar:_:_:password) = break (== ' ') rest
+                (lowLimit,'-':rest) = break (== '-') line
+                (highLimit,' ':watchChar:':':' ':password) = break (== ' ') rest
                 watchCharCount = length $ filter (==watchChar) password
 
 g :: [String] -> Int
-g ss = length $ filter checkValid ss
+g = length . filter checkValid
     where
         checkValid :: String -> Bool
-        checkValid line = (p && not q) || (not p && q)
+        checkValid line = (p && not q) || (not p && q)  --xor
             where
-                (firstI,_:rest) = break (== '-') line
-                (secondI,_:watchChar:_:_:password) = break (== ' ') rest
+                (firstI,'-':rest) = break (== '-') line
+                (secondI,' ':watchChar:':':' ':password) = break (== ' ') rest
                 firstC  | read firstI  > length password = ' '
                         | otherwise = password !! (read firstI  - 1)
                 secondC | read secondI > length password = ' '
@@ -26,5 +26,5 @@ g ss = length $ filter checkValid ss
 main = do
     contents <- readFile "Day2/input.txt"
     let ss = lines contents
-    print (f ss)
-    print (g ss)
+    print ("#1: " ++ show (f ss))
+    print ("#2: " ++ show (g ss))
