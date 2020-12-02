0243a3eeffdb3a15c080da8e4247e692a93d28d0
Richard Holmes <richard.gordon.holmes@gmail.com>
2020-12-02 18:09:54 +0000

    Created template file

---
+++ template.hs
@@ -0,0 +1,11 @@
+f :: a -> b
+f = undefined
+
+g :: a -> b
+g = undefined
+
+main = do
+    contents <- readFile "input/FILENAME.txt"
+    let ss = lines contents
+    print ("#1: " ++ show ())
+    print ("#2: " ++ show ())
