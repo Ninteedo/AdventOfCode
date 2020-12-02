0f2c8f744f56bc5ada6293d05d35999d42ba8187
Richard Holmes <richard.gordon.holmes@gmail.com>
2020-12-02 18:11:27 +0000

    Reformatted file structure

--- 1.hs
+++ 1.hs
@@ -11,7 +11,7 @@ g xs = head [ xs!!i * xs!!j * xs!!k |
               xs!!i + xs!!j + xs!!k == 2020 ]
 
 main = do
-    contents <- readFile "Day1/input.txt"
+    contents <- readFile "input/1.txt"
     let ss = lines contents
         xs :: [Int]
         xs = map read ss
